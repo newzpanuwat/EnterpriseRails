6ed0b9e5cfc0b7346077c2174610ef09e06d5ab5
newzpanuwat <pfcreatorit@gmail.com>
2017-03-16 22:24:31 +0700

    Adding devise and create Auth user and comfirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,10 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -51,4 +55,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
